45d552d202639879200f3b507088dd87b5b66338
Victor Ponamarev <vponamrev@gmail.com>
2021-04-24 23:21:29 +0300

    Connector/Create endpoint in progress.

---
+++ src/SmartCharging.Application/Connectors/CreateConnectorRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartCharging.Application.Connectors
+{
+	public sealed class CreateConnectorRequest
+	{
+		[Range(1, int.MaxValue)]
+		public int ChargeStationId { get; init; }
+
+		[Range(1, 5)]
+		public int LineNo { get; init; }
+
+		[Range(0.000001, double.MaxValue)]
+		public decimal MaxCurrentInAmps { get; init; }
+	}
+}
